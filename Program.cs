970a26415b99e750400de0bc0fa6fd29466c7338
wdeming <57711720+wdeming@users.noreply.github.com>
2022-07-24 23:23:21 +0500

    dz9_.2

--- Program.cs
+++ Program.cs
@@ -40,3 +40,29 @@ void Zadacha66(){
     Console.WriteLine(sum);   
 }
 Zadacha66();
+
+
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 3, n = 2 -> A(m,n) = 29
+
+void Zadacha68(){
+    Console.WriteLine();
+    Console.WriteLine("Задача 68:");
+    int m = 3;
+    int n = 2;
+    Console.WriteLine(AckermannRecursion(m, n));
+}
+
+int AckermannRecursion(int m, int n){
+    if (m == 0){
+        return n + 1;
+    }
+    else if (m > 0 && n == 0){
+        return AckermannRecursion(m - 1, 1);
+    }
+    else if (m > 0 && n > 0){
+        return AckermannRecursion(m - 1, AckermannRecursion(m, n - 1));
+    }
+    else return n + 1;
+}
+Zadacha68();
